fc4fbdd9bfdea2000d04f0488276c08a00a312c0
dwiki <dwiki.nuridhuha@gmail.com>
2022-07-07 10:56:26 +0700

    news test

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
+import {useNavigate} from "react-router-dom"
 
 import CardOne from "../components/CardOne";
 import CardTwo from "../components/CardTwo";
 import CardThree from "../components/CardThree";
 
 function News() {
+    const navigation = useNavigate();
     return (
         <div className="mobile-view">
             <div className="fix-btn">
@@ -15,7 +17,13 @@ function News() {
             <div className="container">
                 <div className="header-content">
                     <div className="icon-back">
-                        icon back
+                        <span>
+                            icon back
+                        </span>
+
+                        <span className="logut" onClick={() => navigation("/")}>
+                            log out
+                        </span>
                     </div>
 
                     <div className="title-Page">
